cb38b15533a1eb2825c43ea812c339dc8383d1ed
Tolo Stamford Otieno <otibomb98@gmail.com>
2025-08-29 23:47:33 +0300

    Update index.js

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -1,13 +1,20 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
-const sequelize = new Sequelize(process.env.DATABASE_URL || 'sqlite:./db/feed_sawa.sqlite', { logging: false });
+const sequelize = new Sequelize(process.env.DATABASE_URL || 'sqlite:./db/eco_sawa.sqlite', { logging: false });
 
 const User = require('./User')(sequelize);
 const Organization = require('./Organization')(sequelize);
 const Pickup = require('./Pickup')(sequelize);
+const Subscription = require('./Subscription')(sequelize);
+const Listing = require('./Listing')(sequelize);
+const Notification = require('./Notification')(sequelize);
+const Impact = require('./Impact')(sequelize);
 
 // associations
 Organization.hasMany(Pickup, { foreignKey: 'organizationId' });
 Pickup.belongsTo(Organization, { foreignKey: 'organizationId' });
 
-module.exports = { sequelize, User, Organization, Pickup };
+User.hasMany(Subscription, { foreignKey: 'userId' });
+User.hasMany(Notification, { foreignKey: 'userId' });
+
+module.exports = { sequelize, User, Organization, Pickup, Subscription, Listing, Notification, Impact };
