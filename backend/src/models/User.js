7d15e736d1822713717e866ea52e0bbd297add7d
Tolo Stamford Otieno <otibomb98@gmail.com>
2025-08-29 23:41:07 +0300

    Update User.js

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,8 +1,27 @@
 const { DataTypes } = require('sequelize');
-module.exports = (sequelize) => sequelize.define('User', {
-  id: { type: DataTypes.STRING, primaryKey: true },
-  name: DataTypes.STRING,
-  role: DataTypes.ENUM('donor','charity','admin'),
-  phone: DataTypes.STRING,
-  email: DataTypes.STRING
-}, { timestamps: true });
+const bcrypt = require('bcrypt');
+
+module.exports = (sequelize) => {
+  const User = sequelize.define('User', {
+    id: { type: DataTypes.STRING, primaryKey: true },
+    name: DataTypes.STRING,
+    role: DataTypes.ENUM('donor','charity','volunteer','admin'),
+    phone: DataTypes.STRING,
+    email: { type: DataTypes.STRING, unique: true },
+    passwordHash: DataTypes.STRING,
+    reputationScore: { type: DataTypes.FLOAT, defaultValue: 0 },
+    locationLat: DataTypes.FLOAT,
+    locationLng: DataTypes.FLOAT
+  }, { timestamps: true });
+
+  User.prototype.setPassword = async function(password) {
+    this.passwordHash = await bcrypt.hash(password, 10);
+  };
+
+  User.prototype.validatePassword = async function(password) {
+    return bcrypt.compare(password, this.passwordHash || '');
+  };
+
+  return User;
+};
+
